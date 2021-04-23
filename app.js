9942373dd75ac442bf3528d6f1399b607f071ada
Pao <paoman12.emo@gmail.com>
2021-04-23 15:59:58 +0700

    test4

--- app.js
+++ app.js
@@ -19,28 +19,29 @@ app.use(bodyParser.json())
 //Message
 
 app.post('/webhook', (req, res) => {
-  let reply_token = req.body.events[0].replyToken
-  reply(reply_token)
+  let reply_token = req.body.events[0].replyToken;
+  let msg = req.body.events[0].message.text;
+  reply(reply_token, msg);
   res.sendStatus(200)
 })
 
 
-function reply(reply_token) {
+function reply(reply_token, msg) {
   let headers = {
       'Content-Type': 'application/json',
       'Authorization': 'Bearer {MFUej68ETDOmnN95+n7dOkr9SGQ8bPw9mn9C4RmlE1wud2zkVcAHbzK7ibC6+mHC6tcWSL6LVKgxU5Mg5i+juHoLGbKxfB5pJmquyre71iSSs886P3KB7wMWVargRO1aEEoGeWhrpGhv2aArMD7U0AdB04t89/1O/w1cDnyilFU=}'
   }
-  let body = JSON.stringify({
-      replyToken: reply_token,
-      messages: [{
-          type: 'text',
-          text: 'Hello'
-      },
-      {
-          type: 'text',
-          text: 'How are you?'
-      }]
-  })
+    if(msg === 'Crypto'){
+            let body = JSON.stringify({
+              replyToken: reply_token,
+              messages: [{
+                  type: 'text',
+                  text: "Ok let's see."
+              }]
+          })
+    }
+
+ 
   request.post({
       url: 'https://api.line.me/v2/bot/message/reply',
       headers: headers,
@@ -50,28 +51,28 @@ function reply(reply_token) {
   });
 }
 
-// const requestOptions = {
-//   method: 'GET',
-//   uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
-//   qs: {
-//     'start': '1',
-//     'limit': '5000',
-//     'convert': 'THB',
-//      'price_min' : 0,
-//      'price_max' : 10000000000
-//   },
-//   headers: {
-//     'X-CMC_PRO_API_KEY': process.env.API_KEY
-//   },
-//   json: true,
-//   gzip: true
-// };
+const requestOptions = {
+  method: 'GET',
+  uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+  qs: {
+    'start': '1',
+    'limit': '5000',
+    'convert': 'THB',
+     'price_min' : 0,
+     'price_max' : 10000000000
+  },
+  headers: {
+    'X-CMC_PRO_API_KEY': process.env.API_KEY
+  },
+  json: true,
+  gzip: true
+};
 
-// rp(requestOptions).then(response => {
-//   console.log(response);
-// }).catch((err) => {
-//   console.log('API call error:', err.message);
-// });
+rp(requestOptions).then(response => {
+  console.log(response);
+}).catch((err) => {
+  console.log('API call error:', err.message);
+});
 
 
 
