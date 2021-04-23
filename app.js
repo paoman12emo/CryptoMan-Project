ca8b9d0c25fbb64ae7b1a77b8402095f57bc1787
Pao <paoman12.emo@gmail.com>
2021-04-23 15:29:08 +0700

    test3

--- app.js
+++ app.js
@@ -19,36 +19,36 @@ app.use(express.json());
 //Message
 
 app.post('/webhook', (req, res) => {
-  // let reply_token = req.body.events[0].replyToken
-  // reply(reply_token)
+  let reply_token = req.body.events[0].replyToken
+  reply(reply_token)
   res.sendStatus(200)
 })
 
 
-// function reply(reply_token) {
-//   let headers = {
-//       'Content-Type': 'application/json',
-//       'Authorization': 'Bearer {process.env.CHANNEL_TOKEN}'
-//   }
-//   let body = JSON.stringify({
-//       replyToken: reply_token,
-//       messages: [{
-//           type: 'text',
-//           text: 'Hello'
-//       },
-//       {
-//           type: 'text',
-//           text: 'How are you?'
-//       }]
-//   })
-//   request.post({
-//       url: 'https://api.line.me/v2/bot/message/reply',
-//       headers: headers,
-//       body: body
-//   }, (err, res, body) => {
-//       console.log('status = ' + res.statusCode);
-//   });
-// }
+function reply(reply_token) {
+  let headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer {process.env.CHANNEL_TOKEN}'
+  }
+  let body = JSON.stringify({
+      replyToken: reply_token,
+      messages: [{
+          type: 'text',
+          text: 'Hello'
+      },
+      {
+          type: 'text',
+          text: 'How are you?'
+      }]
+  })
+  request.post({
+      url: 'https://api.line.me/v2/bot/message/reply',
+      headers: headers,
+      body: body
+  }, (err, res, body) => {
+      console.log('status = ' + res.statusCode);
+  });
+}
 
 // const requestOptions = {
 //   method: 'GET',
