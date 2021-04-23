0a10cc42148ee3ea84862153093a7cb1db038f92
Pao <paoman12.emo@gmail.com>
2021-04-23 15:47:43 +0700

    test4

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json())
 
 //Message
 
-app.get('/webhook', (req, res) => {
+app.post('/webhook', (req, res) => {
   let reply_token = req.body.events[0].replyToken
   reply(reply_token)
   res.sendStatus(200)
