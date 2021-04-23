af8c91f108c6de05280e93912e5d9fcde1faaac5
Pao <paoman12.emo@gmail.com>
2021-04-23 19:38:11 +0700

    Demo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-require('dotenv').config()
-
+const dotenv = require("dotenv");
+dotenv.config()
 const express = require("express");
 const bodyParser = require('body-parser')
 const request = require('request')
@@ -55,7 +55,7 @@ function reply(reply_token,name,price) {
 
   let headers = {
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer {MFUej68ETDOmnN95+n7dOkr9SGQ8bPw9mn9C4RmlE1wud2zkVcAHbzK7ibC6+mHC6tcWSL6LVKgxU5Mg5i+juHoLGbKxfB5pJmquyre71iSSs886P3KB7wMWVargRO1aEEoGeWhrpGhv2aArMD7U0AdB04t89/1O/w1cDnyilFU=}'
+      'Authorization': 'Bearer {process.env.CHANNEL_TOKEN}'
   }
  let body = JSON.stringify({
             replyToken: reply_token,
