153d133aca48828164649629018c99c498c7ec72
Pao <paoman12.emo@gmail.com>
2021-04-24 12:05:41 +0700

    test

--- src/checkword.js
+++ src/checkword.js
@@ -2,10 +2,7 @@ const changeCoinName =  require("./changeCoinName.js");
 
 function checkWord(msg){
 
-    if(msg.substring(0,6) === "ดูราคา"){
-        return changeCoinName(msg.substring(6,msg.length));
-    }
-    else if(msg.substring(0,6) === "ดูราคา "){
+    if(msg.substring(0,6) === "ดูราคา "){
         return changeCoinName(msg.split(" "));  
     }
     else{
@@ -15,3 +12,8 @@ function checkWord(msg){
 }
 
 module.exports = checkWord;
+
+// if(msg.substring(0,6) === "ดูราคา"){
+//     return changeCoinName(msg.substring(6,msg.length));
+// }
+// else
