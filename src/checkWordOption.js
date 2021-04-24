7c6c0cf1f80352e2d9400aa6f1f396e06d4b34f7
Pao <paoman12.emo@gmail.com>
2021-04-24 16:53:53 +0700

    test

--- src/checkWordOption.js
+++ src/checkWordOption.js
@@ -13,7 +13,10 @@ let msg = char.replace(/\s/g, '');
         } 
     else if(msg.substring(3,7) === "ราคา"){
         return changeCoinName(msg.substring(7,msg.length));  
-    }    
+    }
+    else if(msg.substring(3,11) === "ใช้ยังไง"){
+        return changeCoinName(msg.substring(7,msg.length));  
+    }     
     else{
         return changeCoinName(msg.substring(3,msg.length)) 
         }
