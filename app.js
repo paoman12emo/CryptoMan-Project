bc46c45e2396baf419a563d8d3a4c3ec7334790e
Pao <paoman12.emo@gmail.com>
2021-04-24 16:11:13 +0700

    test

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ if(status!= "join"){
   const msg = req.body.events[0].message.text;
   console.log(msg.substring(0,2));
 
-  if(msg.substring(0,2)==="cm"){
+  if(msg.substring(0,2)==="cm"|| msg.substring(0,2)==="Cm"){
     const msg = req.body.events[0].message.text;
     const sender = req.body.events[0].source.groupId?req.body.events[0].source.groupId:req.body.events[0].source.userId
     console.log("ok");
