e85d176ff594a29dede0afdc0f34da6efe064407
Pao <paoman12.emo@gmail.com>
2021-04-24 15:09:33 +0700

    test

--- src/queryCoin.js
+++ src/queryCoin.js
@@ -18,15 +18,15 @@ function queryCoin(coinName,sender){
       
       request(options, function (error, response, body) {
        if(!error){
-        let coinInfo = JSON.parse(body);
+        const coinInfo = JSON.parse(body);
         
-        let name = Object.keys(coinInfo)[0];
+        const name = Object.keys(coinInfo)[0];
   
-        let price = coinInfo[name].thb;
+        const price = coinInfo[name].thb;
   
-        let change = coinInfo[name].thb_24h_change;
+        const change = coinInfo[name].thb_24h_change;
   
-        let vol = coinInfo[name].thb_24h_vol;
+        const vol = coinInfo[name].thb_24h_vol;
   
         reply(sender,name,price,change,vol);
 
