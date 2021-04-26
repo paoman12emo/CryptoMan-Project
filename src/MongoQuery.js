a945932020998edd60318793012a5d4285765958
Pao <paoman12.emo@gmail.com>
2021-04-26 22:22:13 +0700

    demo

--- src/MongoQuery.js
+++ src/MongoQuery.js
@@ -16,7 +16,7 @@ const {fallBack} = require("./replyModule.js");
     useCreateIndex: true
   });
 
-  Coin.find({$or:[{coinShortName : name},{coinFulltName : name}]},(err,res)=>{
+  Coin.find({$or:[{coinShortName : name},{coinFullName : name}]},(err,res)=>{
     let coinData = res[0];
  
     if(coinData==undefined){
--- test.js
+++ test.js
@@ -1,64 +1,64 @@
-// const mongoose = require('mongoose');
-// const Coin = require("./Model/Model.js");
+const mongoose = require('mongoose');
+const Coin = require("./Model/Model.js");
 
 
-// mongoose.connect('mongodb+srv://paoman12emo:paoman12pao@cluster0.mf24n.mongodb.net/CryptoMan?retryWrites=true&w=majority', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false,
-//     useCreateIndex: true
-//   });
+mongoose.connect('mongodb+srv://paoman12emo:paoman12pao@cluster0.mf24n.mongodb.net/CryptoMan?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  });
 
-// let coin = new Coin({
-//   coinFullName: "THETA" ,
-//   coinShortName: "THETA",
-//   URL: "https://coinmarketcap.com/currencies/THETA/"
-// })
+let coin = new Coin({
+  coinFullName: "FILECOIN" ,
+  coinShortName: "FIL",
+  URL: "https://coinmarketcap.com/currencies/filecoin/"
+})
 
-// console.log(coin);
-// coin.save((err)=>{
-//   if(!err){
-//     console.log("ok");
-//   }else{
-//     console.log(err);
-//   }
-// })
-
-
-
-
-
-const request = require('request');
-
-
-function queryCoin(coinName){
-
-  try{
-   const options = {
-     method: 'GET',
-     url: 'https://coingecko.p.rapidapi.com/simple/price',
-     qs: {ids: coinName, vs_currencies: "THB",
-          include_24hr_change: 'true',
-          include_24hr_vol: 'true'
-          },
-     headers: {
-       'x-rapidapi-key': '6c6939db0amsh5ec1aff2cab3017p199644jsn6945f7f35b64',
-       'x-rapidapi-host': 'coingecko.p.rapidapi.com',
-       useQueryString: true
-     }
-   };  
-   request(options, function (err, response, body) {
- 
-     const coinInfo = JSON.parse(body);
-  
-    console.log(coinInfo);
-
-});
+console.log(coin);
+coin.save((err)=>{
+  if(!err){
+    console.log("ok");
+  }else{
+    console.log(err);
   }
- catch(err){
-  
-   console.log(err);
- }
-}
+})
 
-queryCoin("FILECOIN");
+
+
+
+
+// const request = require('request');
+
+
+// function queryCoin(coinName){
+
+//   try{
+//    const options = {
+//      method: 'GET',
+//      url: 'https://coingecko.p.rapidapi.com/simple/price',
+//      qs: {ids: coinName, vs_currencies: "THB",
+//           include_24hr_change: 'true',
+//           include_24hr_vol: 'true'
+//           },
+//      headers: {
+//        'x-rapidapi-key': '6c6939db0amsh5ec1aff2cab3017p199644jsn6945f7f35b64',
+//        'x-rapidapi-host': 'coingecko.p.rapidapi.com',
+//        useQueryString: true
+//      }
+//    };  
+//    request(options, function (err, response, body) {
+ 
+//      const coinInfo = JSON.parse(body);
+  
+//     console.log(coinInfo);
+
+// });
+//   }
+//  catch(err){
+  
+//    console.log(err);
+//  }
+// }
+
+// queryCoin("FILECOIN");
