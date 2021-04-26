f63f504c0893d627d62561d87bb0821e3725369e
Pao <paoman12.emo@gmail.com>
2021-04-26 15:29:27 +0700

    test SQL

--- src/SQL.js
+++ src/SQL.js
@@ -2,7 +2,7 @@ const mysql      = require('mysql');
 
 function queryName(name){
  const connection = mysql.createConnection({
-        host     : 'localhost',
+        host     : '127.0.0.1',
         user     : 'root',
         port     : '3306',
         database : 'cryptoman',
