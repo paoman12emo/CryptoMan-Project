497aa4d5748ffd5486606c29cd657f1edc526f3f
Pao <paoman12.emo@gmail.com>
2021-04-26 21:04:01 +0700

    test

--- src/queryCoin.js
+++ src/queryCoin.js
@@ -4,51 +4,51 @@ const request = require('request');
 
 function queryCoin(coinName,sender,url,cur){
 
-   
+   try{
     const options = {
-        method: 'GET',
-        url: 'https://coingecko.p.rapidapi.com/simple/price',
-        qs: {ids: coinName, vs_currencies: cur,
-             include_24hr_change: 'true',
-             include_24hr_vol: 'true'
-             },
-        headers: {
-          'x-rapidapi-key': '6c6939db0amsh5ec1aff2cab3017p199644jsn6945f7f35b64',
-          'x-rapidapi-host': 'coingecko.p.rapidapi.com',
-          useQueryString: true
-        }
-      };
-  try{    
-      request(options, function (err, response, body) {
+      method: 'GET',
+      url: 'https://coingecko.p.rapidapi.com/simple/price',
+      qs: {ids: coinName, vs_currencies: cur,
+           include_24hr_change: 'true',
+           include_24hr_vol: 'true'
+           },
+      headers: {
+        'x-rapidapi-key': '6c6939db0amsh5ec1aff2cab3017p199644jsn6945f7f35b64',
+        'x-rapidapi-host': 'coingecko.p.rapidapi.com',
+        useQueryString: true
+      }
+    };  
+    request(options, function (err, response, body) {
+  
+      const coinInfo = JSON.parse(body);
+   
+      const name = Object.keys(coinInfo)[0]; 
+      console.log(name);
+
+      const price = coinInfo[name].thb?coinInfo[name].thb:coinInfo[name].usd;
+      
+      const change = coinInfo[name].thb?coinInfo[name].thb_24h_change:coinInfo[name].usd_24h_change;
+
+      const vol = coinInfo[name].thb?coinInfo[name].thb_24h_vol:coinInfo[name].usd_24h_vol;
+
+      let us = coinInfo[name].thb?"฿":"$"
     
-        const coinInfo = JSON.parse(body);
-     
-        const name = Object.keys(coinInfo)[0]; 
-        console.log(name);
 
-        const price = coinInfo[name].thb?coinInfo[name].thb:coinInfo[name].usd;
-        
-        const change = coinInfo[name].thb?coinInfo[name].thb_24h_change:coinInfo[name].usd_24h_change;
-  
-        const vol = coinInfo[name].thb?coinInfo[name].thb_24h_vol:coinInfo[name].usd_24h_vol;
+   if(name=== undefined || price===undefined){
 
-        let us = coinInfo[name].thb?"฿":"$"
-      
- 
-     if(name=== undefined || price===undefined){
-
-      fallBack(sender)
-     }else{
-      
-      reply(sender,name,price,change,vol,url,us);
-     }
-  
-      });
-  
-  }
-  catch(err){
     fallBack(sender)
+   }else{
+    
+    reply(sender,name,price,change,vol,url,us);
+   }
+
+});
+   }
+  catch(err){
     console.log(err);
+  }
+  finally{
+    fallBack(sender)
   }
 }
 
