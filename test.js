7a6e6c2c60ddca4e72bfcd6ec41aa68fa19e0a86
Pao <paoman12.emo@gmail.com>
2021-04-26 20:31:24 +0700

    Demo

--- test.js
+++ test.js
@@ -1,31 +1,33 @@
-const mongoose = require('mongoose');
-const Coin = require("./Model/Model.js");
+// const mongoose = require('mongoose');
+// const Coin = require("./Model/Model.js");
 
 
-mongoose.connect('mongodb+srv://paoman12emo:Paoman12pao@cluster0.mf24n.mongodb.net/CryptoMan?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-  });
+// mongoose.connect('mongodb+srv://paoman12emo:Paoman12pao@cluster0.mf24n.mongodb.net/CryptoMan?retryWrites=true&w=majority', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//     useCreateIndex: true
+//   });
 
-let coin = new Coin({
-  coinFullName: "NEO" ,
-  coinShortName: "NEO",
-  URL: "https://coinmarketcap.com/currencies/NEO/"
-})
+// let coin = new Coin({
+//   coinFullName: "NEO" ,
+//   coinShortName: "NEO",
+//   URL: "https://coinmarketcap.com/currencies/NEO/"
+// })
 
-console.log(coin);
-coin.save((err)=>{
-  if(!err){
-    console.log("ok");
-  }else{
-    console.log(err);
-  }
-})
+// console.log(coin);
+// coin.save((err)=>{
+//   if(!err){
+//     console.log("ok");
+//   }else{
+//     console.log(err);
+//   }
+// })
+require('dotenv').config()
 
+let urlDB ='mongodb+srv://paoman12emo:'+process.env.DB_KEY+'@cluster0.mf24n.mongodb.net/CryptoMan?retryWrites=true&w=majority' 
 
-
+console.log(typeof(urlDB));
 
 
 
