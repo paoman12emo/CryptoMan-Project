94a20c3c95de8e5876d3222cbcf4a65e3eb20ed9
Pao <paoman12.emo@gmail.com>
2021-04-30 12:12:56 +0700

    test

---
+++ src/ScheduleNews.js
@@ -0,0 +1,52 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const {Sender} = require("../Model/Model.js");
+const cron = require('node-cron');
+const mongoose = require('mongoose');
+
+const {reportNews}= require('./replyModule.js');
+
+
+
+cron.schedule('20 * * * * *', async () => {
+
+let urlDB ='mongodb+srv://paoman12emo:paoman12pao@cluster0.mf24n.mongodb.net/CryptoMan?retryWrites=true&w=majority' 
+
+mongoose.connect(urlDB, {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+useFindAndModify: false,
+useCreateIndex: true
+});
+  try{
+    const response =  await axios.get(`https://www.coingecko.com/th/news`)
+    const html = response.data
+    const $ = cheerio.load(html)
+    const news = $('.tw-text-xl a[target]');
+    const newsLists = []
+
+    news.each((index, element)=>{
+        newsLists.push({
+            text: $(element).text(), 
+            url: $(element).attr('href'), 
+          });
+        });
+
+
+    Sender.find({},(err,res)=>{
+      res.forEach((item)=>{
+        let sender = item.sender;
+
+        await reportNews(sender,newsLists);
+
+       console.log(sender);
+      })
+    })
+  
+  
+  }
+ catch(err){
+   console.log(err);
+ }
+
+});
